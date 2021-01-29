f723275571d2123921c6b3c6247b6e4ee61edd9d
Gramos <imeri.gramos@gmail.com>
2021-01-29 14:01:11 +0100

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Renders default screen", () => {
+  test("Should display input and button", () => {
+    render(<App />);
+
+    const buttonAdd = screen.getByRole("button", { name: /add/i });
+    expect(buttonAdd).toBeInTheDocument();
+
+    const input = screen.getByPlaceholderText(/add todo/i);
+    expect(input).toBeInTheDocument();
+  });
+
+  test("Should display a empty todolist", () => {
+    render(<App />);
+
+    const todos = screen.getByTestId("todos");
+    expect(todos.children.length).toBe(0);
+  });
+
+  test("Add a todo", () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText(/add todo/i);
+    fireEvent.change(input, {
+      target: { value: "Buy bread" },
+    });
+
+    const buttonAdd = screen.getByRole("button", { name: /add/i });
+    fireEvent.click(buttonAdd);
+    expect(screen.getByText(/Buy bread/i)).toBeInTheDocument();
+  });
+
+  test("Check & uncheck a todo", () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText(/add todo/i);
+    fireEvent.change(input, {
+      target: { value: /buy bread/i },
+    });
+
+    const buttonAdd = screen.getByRole("button", { name: /add/i });
+    fireEvent.click(buttonAdd);
+
+    const checkbox = screen.getByLabelText(/buy bread/i);
+    fireEvent.click(checkbox);
+    expect(screen.getByText(/buy bread/i)).toHaveStyle({
+      textDecoration: "line-through",
+    });
+    fireEvent.click(checkbox);
+    expect(screen.getByText(/buy bread/i)).toHaveStyle({
+      textDecoration: undefined,
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { uid } from "uid";
+import "./App.css";
+
+interface Todo {
+  id: string;
+  text: string;
+  checked: boolean;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    setTodos((prevState) => [
+      ...prevState,
+      { id: uid(), text, checked: false },
+    ]);
+    setText("");
+  };
+
+  const checkTodo = (id: string) => {
+    const newListTodo = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !todo.checked;
+      }
+      return todo;
+    });
+    setTodos(newListTodo);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add todo..."
+      />
+      <ul data-testid="todos">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label
+              htmlFor="toggle"
+              style={{
+                textDecoration: todo.checked ? "line-through" : undefined,
+              }}
+            >
+              {todo.text}
+              <input
+                id="toggle"
+                type="checkbox"
+                data-testid="checkbox"
+                onChange={() => checkTodo(todo.id)}
+              />
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button type="submit" onClick={addTodo} disabled={!text.length}>
+        Add
+      </button>
     </div>
   );
 }
